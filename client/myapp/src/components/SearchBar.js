e708de1abbb728e31c78c3bf72730e941caa3d2a
Tyler Campbell <tcampb30@gmail.com>
2018-02-11 14:26:13 -0500

    Updated list item styles

--- client/myapp/src/components/SearchBar.js
+++ client/myapp/src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import logo from './logo.png';
 
 class SearchBar extends Component {
     constructor(props){
@@ -13,15 +14,17 @@ class SearchBar extends Component {
     }
 
     render() {
+        const listItemInfoStyle = {margin: '0px', paddingBottom: '3px'};
         const iconStyle = {fontSize: '.8125rem', color: '#b0adab'};
         const divStyle = {paddingBottom: '3px'};
-        const listItems = this.state.filteredList.map(record => <li>{record.company}</li>)
+        const listItems = this.state.filteredList.map(record => <li><div><img src={logo} width="36px" height="36px" /></div> 
+        <div><h4 style={listItemInfoStyle}>{record.company}</h4><h6 style={listItemInfoStyle}>{record.objectType}</h6></div></li>)
         return <div className="searchbar-wrapper">
                 <div className="searchbar">
                     <input type="text" placeholder="Search..." onFocus={this.clickHandler} onBlur={this.clickHandler} onChange={this.searchHandler} />
                 </div>
                 <div style={divStyle}><i className="fas fa-search" style={iconStyle}></i></div>
-                <div className="searchbar-dropdown" data-dropdown>
+                <div className="searchbar-dropdown hide" data-dropdown>
                 <ul>
                     {listItems}
                 </ul>

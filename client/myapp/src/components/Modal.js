0da9eeb56ef6ce6ef171e302586bb381f9f5aef2
Tyler Campbell <tcampb30@gmail.com>
2018-02-12 06:58:04 -0500

    Added form sections and additional styling

--- client/myapp/src/components/Modal.js
+++ client/myapp/src/components/Modal.js
@@ -6,8 +6,34 @@ const closeModal = () => {
 }
 
 const Modal = () => {
-    const targetForm = <form>
-                        <div className="ui-input"><span className="required">Target Name</span><input type="text" /></div>
+    const targetForm =
+                        <form>
+                        <h3 className="form-legend">Information</h3>
+                        <div className="col-left">
+                        <div className="ui-input"><span className="required"></span><span>Target Name</span>
+                        <div className="clearfix"></div>
+                        <input type="text" required /></div>
+                        <div className="ui-input"><span></span><span>Industry</span>
+                        <div className="clearfix"></div>
+                        <input type="text"  /></div>
+                        <div className="ui-input"><span></span><span>Annual Revenue</span>
+                        <div className="clearfix"></div>
+                        <input type="number"  /></div>
+                        </div>
+                        <div className="col-right">
+                        <div className="ui-input"><span></span><span>Employees</span>
+                        <div className="clearfix"></div>
+                        <input type="number"  /></div>
+                        <div className="ui-input"><span></span><span>Location</span>
+                        <div className="clearfix"></div>
+                        <input type="number"  /></div>
+                        <div className="ui-input"><span></span><span>Deal Lead</span>
+                        <div className="clearfix"></div>
+                        <input type="text"  /></div>
+                        </div>
+                        <div className="clearfix"></div>
+                        <h3 className="form-legend">Related Contacts</h3>
+
                         </form>
                          
 
